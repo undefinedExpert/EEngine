64aa983884a04ec30fce8f2f87213ce5573ed194
Emanuel Slotwinski <ask@eslotwinski.com>
2016-02-03 23:45:19 +0100

    adding working method whose adds velocity to precised element

--- app/game/core/core.js
+++ app/game/core/core.js
@@ -3,26 +3,23 @@
 import THREE from 'three.js'; // 3D library
 import CANNON from 'cannon'; // Physics Library
 
-
-//Setting namespaces for values
-
 //Three.js
 var camera, scene, renderer;
 
 //Cannon (physic engine)
 var geometry, material, mesh;
-var world, mass, body, shape, timeStep = 1 / 60;
+var world, mass, square, shape, timeStep = 1 / 60;
+
 
 
 var core = {
 
       /**
-       * @function init
-         * @desc Init whole scene with assets
+       * @method init
+       * @desc Init whole scene with assets
        */
       init: function () {
             scene = new THREE.Scene();
-            // Adding lighting
             var directionalLight = new THREE.DirectionalLight(0xffeedd);
             directionalLight.position.set(0, 0, 1).normalize();
             scene.add(directionalLight);
@@ -45,6 +42,14 @@ var core = {
 
             document.body.appendChild(renderer.domElement);
 
+
+            //Interaction with square button
+            var addSpeedButton = document.getElementById('addSpeed');
+            addSpeedButton.addEventListener('click', () => {
+                  this.addMovement(square, 5);
+            });
+
+
             /*
              * Init other functions
              * cannon() - initialize configurations for physics (cannon.js)
@@ -58,7 +63,10 @@ var core = {
             this.updatePhysics();
             this.render();
       },
-
+      /**
+       * @method cannon
+       * @desc Initialize whole physics for scene and it's objects
+       */
       cannon: function () {
             //Cannon init
             world = new CANNON.World();
@@ -69,22 +77,30 @@ var core = {
             //Dodaje nowy "kolidator" dla body nadaje mu wartosci masy ksztaltu obrotu etc i by na koncu dodac go to "world", czyli sceny
             shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
             mass = 1;
-            body = new CANNON.Body({
+            square = new CANNON.Body({
                   mass: 1
             });
-            body.addShape(shape);
-            body.angularVelocity.set(25, 15, 15);
-            body.angularDamping = 0.5;
-            world.addBody(body);
-      },
-      animate: function () {
-            //requestAnimationFrame(this.animate.bind(this.()));
-            requestAnimationFrame(this.animate.bind(this));
+            square.addShape(shape);
+            square.angularVelocity.set(0, 0, 0);
 
-            //Aktualizuje fizyke
+            square.angularDamping = 0.5;
+            world.addBody(square);
+      },
+      /**
+       * @method animate
+       * @desc Three.js function which updates scene
+       * @see {@link init}
+       */
+      animate: function () {
+            requestAnimationFrame(this.animate.bind(this));
             this.updatePhysics();
             this.render();
       },
+      /**
+       * @method updatePhysics
+       * @desc CommonJS function which updates physics of all objects and world
+       * @see {@link animate, @link cannon}
+       */
       updatePhysics: function () {
             // Step the physics world
             world.step(timeStep);
@@ -93,11 +109,31 @@ var core = {
             /*
              * Tutaj aktualizuje pozycje mecha z wartoscia ktora jest sprecyzowana w core.cannon()
              * */
-            mesh.position.copy(body.position);
-            mesh.quaternion.copy(body.quaternion);
+            mesh.position.copy(square.position);
+            mesh.quaternion.copy(square.quaternion);
       },
+      /**
+       * @method render
+       * @desc three.js Method whose render scene
+       * @see {@link init}
+       */
       render: function () {
             renderer.render(scene, camera);
+      },
+
+      /**
+        * @function addMovment
+        * @desc opens a modal window to display a message
+        * @param string $msg - the message to be displayed
+        * @return bool - success or failure
+      */
+
+      addMovement: function(element, force) {
+
+          let meshPosition = mesh.position.copy(element.position);
+          let meshQuaternion = mesh.quaternion.copy(element.quaternion);
+
+          element.angularVelocity.y = element.angularVelocity.y + force;
       }
 
 };
